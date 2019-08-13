7ab18bce50b29de453b23501fb6b20f0f3d899b2
Dirk <koritnik@werkraum-media.de>
2019-08-13 09:32:46 +0200

    Removed old ImportCommand

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,8 +1,5 @@
 <?php
 return [
-    'events:import‚' => [
-        'class' => \Wrm\Events\Command\ImportCommand::class
-    ],
     'events:destinationdataimport‚' => [
         'class' => \Wrm\Events\Command\DestinationDataImportCommand::class
     ]
