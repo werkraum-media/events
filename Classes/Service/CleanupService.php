ca4d7d3a016e20d0d43cd15c0ebd02697324f94b
Daniel Siepmann <coding@daniel-siepmann.de>
2019-09-17 17:14:28 +0200

    Add basic cleanup command
    Allows to remove all existing records regarding events.
    It will search for all pages providing either organizer or events, and
    will delete both types of records.
    
    As DataHandler is used for deletion, there is already logging within
    TYPO3, as well as recursive deletion for dates and sys_file_references.
    
    To speedup for large data sets, truncate organizer and dates, as they
    don't have recursive deletions, e.g. to sys_file_references. Also add
    keys to database.

--- Classes/Service/CleanupService.php
+++ Classes/Service/CleanupService.php
@@ -2,39 +2,74 @@
 
 namespace Wrm\Events\Service;
 
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Log\LogManager;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
-class CleanupService {
+class CleanupService
+{
+    public function deleteAllData()
+    {
+        $this->truncateTables(... [
+            'tx_events_domain_model_date',
+            'tx_events_domain_model_organizer',
+        ]);
 
-    /**
-     * Cleanup Service constructor.
-     * @param ConfigurationManager $configurationManager
-     * @param ObjectManager $objectManager
-     */
-    public function __construct(
-        ConfigurationManager $configurationManager,
-        ObjectManager $objectManager
-    ) {
-
-        // Get Typoscript Settings
-        $this->settings = $this->configurationManager->getConfiguration(
-            ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS,
-            'Events',
-            'Pi1'
-        );
-
-
-        $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
-        $this->logger->info('Event Cleanup Service');
+        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+        /* @var DataHandler $dataHandler */
+        $dataHandler->start([], $this->getDeletionStructure([
+            'tx_events_domain_model_event',
+        ]));
+        $dataHandler->process_cmdmap();
     }
 
-    public function doCleanup() {
-        
-        // To be done
-        // Hmpf
+    private function truncateTables(string ...$tableNames): void
+    {
+        foreach ($tableNames as $tableName) {
+            GeneralUtility::makeInstance(ConnectionPool::class)
+                ->getConnectionForTable($tableName)
+                ->truncate($tableName);
+        }
+    }
+
+    private function getDeletionStructure(array $tableNames): array
+    {
+        $structure = [];
+
+        foreach ($tableNames as $tableName) {
+            $structure = array_merge($structure, $this->getDeletionStructureForTable($tableName));
+        }
+
+        return $structure;
+    }
+
+    private function getDeletionStructureForTable(string $tableName): array
+    {
+        $dataStructure = [$tableName=> []];
+
+        foreach ($this->getRecordsToDelete($tableName) as $recordToDelete) {
+            $dataStructure[$tableName][$recordToDelete] = ['delete' => 1];
+        }
+
+        return $dataStructure;
+    }
+
+    private function getRecordsToDelete(string $tableName): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable($tableName)
+            ->createQueryBuilder();
+
+        /* @var QueryBuilder $queryBuilder */
+        $records = $queryBuilder->select('uid')
+            ->from($tableName)
+            ->execute()
+            ->fetchAll();
+
+        return array_map(function (array $record) {
+            return $record['uid'];
+        }, $records);
     }
 }
--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -2,5 +2,8 @@
 return [
     'events:destinationdataimport‚' => [
         'class' => \Wrm\Events\Command\DestinationDataImportCommand::class
-    ]
+    ],
+    'events:removeAll' => [
+        'class' => \Wrm\Events\Command\RemoveAllCommand::class
+    ],
 ];
