906c06c633bd92c421fd0e9795900feb3e9f94d3
Daniel Siepmann <coding@daniel-siepmann.de>
2023-05-22 08:15:51 +0200

    Allow to show only upcoming events
    A new TypoScript option upcoming is added.
    The option can be set to 0 (default) or 1.
    0 behaves the same way as in the past.
    1 turns off the option useMidnight, start and end.
    Only dates with a start date in the future will be shown.
    
    Relates: #10507

---
+++ Tests/Functional/Frontend/DatesTestFixtures/ReturnsUpcomingDates.php
@@ -0,0 +1,43 @@
+<?php
+
+use DateTimeImmutable;
+
+return [
+    'tt_content' => [
+        [
+            'uid' => 1,
+            'pid' => 1,
+            'CType' => 'list',
+            'list_type' => 'events_datelist',
+            'header' => 'Upcoming Dates',
+        ],
+    ],
+    'tx_events_domain_model_event' => [
+        [
+            'uid' => 1,
+            'pid' => 2,
+            'title' => 'Event 1',
+        ],
+        [
+            'uid' => 2,
+            'pid' => 2,
+            'title' => 'Event 2',
+        ],
+    ],
+    'tx_events_domain_model_date' => [
+        [
+            'uid' => 1,
+            'pid' => 2,
+            'event' => 1,
+            'start' => (new DateTimeImmutable())->modify('-5 minutes')->format('U'),
+            'end' => (new DateTimeImmutable())->modify('+5 minutes')->format('U'),
+        ],
+        [
+            'uid' => 2,
+            'pid' => 2,
+            'event' => 2,
+            'start' => (new DateTimeImmutable())->modify('+5 minutes')->format('U'),
+            'end' => (new DateTimeImmutable())->modify('+15 minutes')->format('U'),
+        ],
+    ],
+];
