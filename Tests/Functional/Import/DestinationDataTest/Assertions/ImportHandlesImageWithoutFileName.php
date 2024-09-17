31699a278c55537970348fbd4915965b4d1455a6
Daniel Siepmann <daniel.siepmann@codappix.com>
2024-09-17 10:46:23 +0200

    Handle destination.one files without file name.
    It is possible to have files such as
    `https://dam.destination.one/2675868/3dc0a9dccd0dad46c73e669ece428c634ff8324ea3d01a4858a1c43169deed41/.jpg`.
    Those were not handled correctly yet.
    We now also handle those cases.
    We will generate a hash from the URL as file name in order to still use those files.
    
    Relates: #11396

---
+++ Tests/Functional/Import/DestinationDataTest/Assertions/ImportHandlesImageWithoutFileName.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Resource\File;
+
+return [
+    'sys_file' => [
+        [
+            'uid' => 1,
+            'pid' => 0,
+            'missing' => 0,
+            'storage' => 1,
+            'type' => File::FILETYPE_IMAGE,
+            'identifier' => '/staedte/beispielstadt/events/bf126089c94f95031fa07bf9d7d9b10c3e58aafebdef31f0b60604da13019b8d.jpg',
+            'extension' => 'jpg',
+            'name' => 'bf126089c94f95031fa07bf9d7d9b10c3e58aafebdef31f0b60604da13019b8d.jpg',
+        ],
+    ],
+    'sys_file_reference' => [
+        [
+            'uid' => 1,
+            'pid' => 2,
+            'uid_local' => 1,
+            'uid_foreign' => 1,
+            'tablenames' => 'tx_events_domain_model_event',
+            'fieldname' => 'images',
+            'sorting_foreign' => 1,
+            'title' => null,
+            'description' => null,
+            'alternative' => null,
+        ],
+    ],
+    'sys_file_metadata' => [
+        [
+            'uid' => 1,
+            'pid' => 0,
+            'file' => 1,
+            'title' => '',
+        ],
+    ],
+];
