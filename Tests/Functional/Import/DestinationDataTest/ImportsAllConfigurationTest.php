ab29ee972419d51c590c7a4d6df38f87362a2ad2
Daniel Siepmann <daniel.siepmann@codappix.com>
2024-11-06 07:54:30 +0100

    Further removals

--- Tests/Functional/Import/DestinationDataTest/ImportsAllConfigurationTest.php
+++ Tests/Functional/Import/DestinationDataTest/ImportsAllConfigurationTest.php
@@ -51,7 +51,7 @@ class ImportsAllConfigurationTest extends AbstractTestCase
         self::assertSame('https://dam.destination.one/828118/f13bbf5602ffc406ebae2faa3527654dea84194666bce4925a1ca8bd3f50c5e9/tueftlerzeit-sfz-rudolstadt-jpg.jpg', (string)$requests[2]['request']->getUri());
         self::assertSame('https://dam.destination.one/853436/109ac1cf87913e21b5e2b0ef0cc63d223a14374364952a855746a8e7c3fcfc36/lutherkirche-jpg.jpg', (string)$requests[3]['request']->getUri());
 
-        self::assertSame('http://meta.et4.de/rest.ashx/search/?experience=beispielstadt&licensekey=example-license&type=Event&mode=next_months%2C12&limit=3&template=ET2014A.json&q=name%3A%22Beispiel%22', (string)$requests[0]['request']->getUri());
+        self::assertSame('http://meta.et4.de/rest.ashx/search/?experience=anderestadt&licensekey=example-license&type=Event&mode=next_months%2C12&limit=3&template=ET2014A.json&q=name%3A%22Beispiel2%22', (string)$requests[4]['request']->getUri());
         self::assertSame('https://dam.destination.one/849917/279ac45b3fc701a7197131f627164fffd9f8cc77bc75165e2fc2b864ed606920/theater-rudolstadt_johannes-gei-er_photo-by-lisa-stern_web_-jpg.jpg', (string)$requests[5]['request']->getUri());
         self::assertSame('https://dam.destination.one/828118/f13bbf5602ffc406ebae2faa3527654dea84194666bce4925a1ca8bd3f50c5e9/tueftlerzeit-sfz-rudolstadt-jpg.jpg', (string)$requests[6]['request']->getUri());
         self::assertSame('https://dam.destination.one/853436/109ac1cf87913e21b5e2b0ef0cc63d223a14374364952a855746a8e7c3fcfc36/lutherkirche-jpg.jpg', (string)$requests[7]['request']->getUri());
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,45 +2,9 @@
 
 declare(strict_types=1);
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use WerkraumMedia\Events\Controller\DateController;
-use WerkraumMedia\Events\Controller\EventController;
-
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(function () {
-    ExtensionUtility::configurePlugin(
-        'Events',
-        'DateSearch',
-        [DateController::class => 'search'],
-        [DateController::class => 'search'],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
-
-    ExtensionUtility::configurePlugin(
-        'Events',
-        'DateList',
-        [DateController::class => 'list'],
-        [DateController::class => 'list'],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
-
-    ExtensionUtility::configurePlugin(
-        'Events',
-        'DateShow',
-        [DateController::class => 'show'],
-        [DateController::class => 'show'],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
-
-    ExtensionUtility::configurePlugin(
-        'Events',
-        'Selected',
-        [EventController::class => 'list'],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
-
     if (
         isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['events_category']) === false
         || is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['events_category']) === false
