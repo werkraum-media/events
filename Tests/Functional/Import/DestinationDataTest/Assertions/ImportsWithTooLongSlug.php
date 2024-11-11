cb5a38da9304104bc7ea3b716a13f6b120e2bf9b
Daniel Siepmann <daniel.siepmann@codappix.com>
2024-11-11 07:28:52 +0100

    WIP|Shorten slugs
    Slugs might sometimes become too long.
    This should be handled, but we do not know how right now.
    This is already a test setup for validating the logic.
    
    Relates: #11496

---
+++ Tests/Functional/Import/DestinationDataTest/Assertions/ImportsWithTooLongSlug.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'tx_events_domain_model_event' => [
+        [
+            'uid' => 1,
+            'global_id' => 'e_100347854',
+            'slug' => 'kann-man-noch-guten-gewissens-kinder',
+        ],
+    ],
+    'tx_events_domain_model_date' => [
+        [
+            'uid' => 1,
+            'event' => '1',
+            'slug' => 'kann-man-noch-guten-gewissens-kinder-2099-12-19t14-00-00',
+        ],
+    ],
+];
--- Tests/Functional/Import/DestinationDataTest/ImportsExampleAsExpectedTest.php
+++ Tests/Functional/Import/DestinationDataTest/ImportsExampleAsExpectedTest.php
@@ -94,4 +94,18 @@ class ImportsExampleAsExpectedTest extends AbstractTestCase
         $this->assertPHPDataSet(__DIR__ . '/Assertions/ImportsKeywords.php');
         $this->assertEmptyLog();
     }
+
+    #[Test]
+    public function importsWithTooLongSlug(): void
+    {
+        $this->importPHPDataSet(__DIR__ . '/Fixtures/Database/DefaultImportConfiguration.php');
+        $this->setUpResponses([
+            new Response(200, [], file_get_contents(__DIR__ . '/Fixtures/ResponseWithTooLongSlug.json') ?: ''),
+        ]);
+
+        $this->executeCommand();
+
+        $this->assertPHPDataSet(__DIR__ . '/Assertions/ImportsWithTooLongSlug.php');
+        $this->assertEmptyLog();
+    }
 }
